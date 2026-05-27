d6804678e04b28e86cfc47356fb75d97cdb0e8f5
Afonso-Macarico <up202403501@up.pt>
2026-05-27 11:06:51 +0100

    Add files via upload

---
+++ SVGElements.hpp
@@ -0,0 +1,99 @@
+//! @file shape.hpp
+#ifndef __svg_SVGElements_hpp__
+#define __svg_SVGElements_hpp__
+
+#include "Color.hpp"
+#include "Point.hpp"
+#include "PNGImage.hpp"
+
+namespace svg
+{
+    class SVGElement
+    {
+
+    public:
+        SVGElement();
+        virtual ~SVGElement();
+        virtual void draw(PNGImage &img) const = 0;
+    };
+
+    // Declaration of namespace functions
+    // readSVG -> implement it in readSVG.cpp
+    // convert -> already given (DO NOT CHANGE) in convert.cpp
+
+    void readSVG(const std::string &svg_file,
+                 Point &dimensions,
+                 std::vector<SVGElement *> &svg_elements);
+    void convert(const std::string &svg_file,
+                 const std::string &png_file);
+
+    class Ellipse : public SVGElement
+    {
+    public:
+        Ellipse(const Color &fill, const Point &center, const Point &radius);
+        void draw(PNGImage &img) const override;
+
+    private:
+        Color fill;
+        Point center;
+        Point radius;
+    };
+    class Circle : public SVGElement
+    {
+    public:
+        Circle(const Color &fill, const Point &center, const int &radius);
+        void draw(PNGImage &img) const override;
+
+    private:
+        Color fill;
+        Point center;
+        int radius;
+    };
+    class Polyline : public SVGElement
+    {
+    public:
+        Polyline(const Color &stroke, const std::vector<Point> &points);
+        void draw(PNGImage &img) const override;
+
+    private:
+        Color stroke;
+        std::vector<Point> points;
+    };
+    class Line : public SVGElement
+    {
+    public:
+        Line(const Color &stroke, const Point &start);
+        void draw(PNGImage &img) const override;
+
+    private:
+        Color stroke;
+        Point start;
+        Point end;
+    };
+    class Polygon : public SVGElement
+    {
+    public:
+        Polygon(const Color &fill, const std::vector<Point> &points);
+        void draw(PNGImage &img) const override;
+
+    private:
+        Color fill;
+        std::vector<Point> points;
+    };
+    class Rect : public SVGElement
+    {
+    public:
+        Rect(const Color &fill, const int &width, const int &height, const Point &corner);
+        void draw(PNGImage &img) const override;
+
+    private:
+        Color fill;
+        int width;
+        int height;
+        Point corner;
+    };
+
+
+
+}
+#endif
